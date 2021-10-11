27f5ad74b5fe82a6d12a62330b1eaddb8d4c518d
calvin9150 <wonbae9150@gmail.com>
2021-10-11 17:37:12 +0900

    메인페이지 레이아웃

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,70 @@
 import React from "react";
-import styled from "styled-components";
 
-const Layout = styled.div`
-  width: 300px;
-  height: 450px;
-  border: 1px solid black;
-  margin: 20px;
-`;
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
 
-const Card = (props) => {
+// const Layout = styled.div`
+//   width: 300px;
+//   height: 450px;
+//   border: 1px solid black;
+//   margin: 20px;
+// `;
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: 20,
+  },
+  media: {
+    height: 250,
+  },
+  content: {
+    maxHeight: 300,
+  },
+});
+
+const CardLayout = (props) => {
+  const classes = useStyles();
   return (
-    <>
-      <Layout>응애</Layout>
-    </>
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image="https://smtmap.com/wp-content/uploads/2020/06/%EA%B3%B0-%EA%BF%88.jpg"
+        alt="사진"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          지갑 주인 찾아요
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          className={classes.content}
+          noWrap="true"
+        >
+          지갑안에 1000만원있는데, 주인 찾습니다. 지갑안에 1000만원있는데, 주인
+          찾습니다.지갑안에 1000만원있는데, 주인 찾습니다.지갑안에
+          1000만원있는데, 주인 찾습니다.지갑안에 1000만원있는데, 주인
+          찾습니다.지갑안에 1000만원있는데, 주인 찾습니다.지갑안에
+          1000만원있는데, 주인 찾습니다.지갑안에 1000만원있는데, 주인
+          찾습니다.지갑안에 1000만원있는데, 주인 찾습니다.지갑안에
+          1000만원있는데, 주인 찾습니다.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained" color="primary">
+          수정하기
+        </Button>
+        {/* <Button size="small">Learn More</Button> */}
+      </CardActions>
+    </Card>
   );
 };
 
-export default Card;
+export default CardLayout;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,16 +9,27 @@ const Container = styled.div`
   width: 70vw;
   min-height: 900px;
   background-color: #c0c0c0;
+
+  @media screen and (max-width: 720px) {
+    width: 90vw;
+  }
 `;
 
 const CardsWrappper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  margin: 70px 0;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   width: 90%;
-  background-color: gray;
+  /* background-color: gray; */
+  @media screen and (max-width: 1280px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 820px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 const Main = (props) => {
