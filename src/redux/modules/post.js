a9b6742db886130c8e1bdb0b67cac86e4d935127
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-10-13 20:18:53 +0900

    동건-뷰작업,디테일페이지 새로고침수정

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -4,10 +4,10 @@ import { api } from "../../lib/apis";
 
 //action type
 const ADD_POST = "ADD_POST";
-
+const SET_POST = "SET_POST";
 //actionCreator
 const addPost = createAction(ADD_POST, (post) => ({ post }));
-
+const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
 //initialState
 const initialState = {
   list: [],
@@ -28,6 +28,25 @@ const addPostsMiddleware = (title, phonenumber, contents, location, imgurl) => {
   };
 };
 
+// const getOnePostsMiddleware = () => {
+//   return (dispatch, getState, { history }) => {
+//     api
+//       .get("/contents")
+//       .then((res) => {
+//         const post_list = res.data;
+//         console.log(post_list, "서버에 저장된 데이터")
+//         const _data = getState().posts.list
+//         console.log(_data, "ㅔㅐ")
+//         const _post_list = _data.push(post_list)
+//         console.log(_post_list)
+//         // dispatch(setPost(post_list));
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   };
+// };
+
 // reducer
 export default handleActions(
   {
@@ -35,14 +54,20 @@ export default handleActions(
       produce(state, (draft) => {
         draft.list = action.payload.post;
       }),
+    [SET_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list = action.payload.post_list;
+      }),
   },
+
   initialState
 );
 
 // action creator export
 const actionCreators = {
   addPost,
-  addPostsMiddleware,
+  
+  // getOnePostsMiddleware,
 };
 
 export { actionCreators };
