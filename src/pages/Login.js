83a560d8aef004afb8feae49df632c83dfbce154
calvin9150 <wonbae9150@gmail.com>
2021-10-16 09:30:54 +0900

    로그인페이지 엔터키 이슈 해결결

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,121 +1,123 @@
 import React from "react";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user";
-import {setCookie} from "../shared/Cookie";
+import { setCookie } from "../shared/Cookie";
 import { history } from "../redux/configureStore";
 
-
-
 const Login = (props) => {
-
   const theme = createTheme();
   const dispatch = useDispatch();
 
-  const [username, setUserName] = React.useState('');
-  const [password, setPwd] = React.useState('');
+  const [username, setUserName] = React.useState("");
+  const [password, setPwd] = React.useState("");
 
   const login = () => {
-        // setCookie("username", username, 3);
-        // setCookie("is_login", true, 3)
-        // window.location.href = "/";
+    // setCookie("username", username, 3);
+    // setCookie("is_login", true, 3)
+    // window.location.href = "/";
 
-        console.log(username);       
-        if(username === "" || password === ""){
-            window.alert("아이디 혹은 비밀번호가 공란입니다! 입력해주세요");
-            return;
-        }
+    console.log(username);
+    if (username === "" || password === "") {
+      window.alert("아이디 혹은 비밀번호가 공란입니다! 입력해주세요");
+      return;
+    }
 
-        dispatch(userActions.loginDB(username, password));
-};
+    dispatch(userActions.loginDB(username, password));
+  };
 
+  const onKeyPressPw = (e) => {
+    if (e.key === "Enter") {
+      alert("h");
+      login();
+    }
+  };
 
   return (
     <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "gray" }}>
-          <LockOutlinedIcon/>
-        </Avatar>
-
-        <Typography component="h1" variant="h5">
-          로그인
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="id"
-            label="아이디"
-            name="id"
-            autoComplete="id"
-            placeholder="아이디를 입력하세요"
-            autoFocus
-            onChange={(e) => {
-              setUserName(e.target.value);
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="패스워드"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            placeholder="비밀번호를 입력하세요"
-            onChange={(e) => {
-              setPwd(e.target.value);
-          }}
-          />
-          <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 1.5 }}
-            onClick={() => {
-              console.log("로그인버튼클릭!");
-              login()}}
-          >
-            로그인하기
-          </Button>
+        >
+          <Avatar sx={{ m: 1, bgcolor: "gray" }}>
+            <LockOutlinedIcon />
+          </Avatar>
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 0.1, mb: 2, }}
-            style={{backgroundColor:'#FEE500', color:'#000000'}}
-          >
-            카카오 로그인
-          </Button>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+          <Box noValidate sx={{ mt: 1 }} onKeyPress={onKeyPressPw}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="id"
+              label="아이디"
+              name="id"
+              autoComplete="id"
+              placeholder="아이디를 입력하세요"
+              autoFocus
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="패스워드"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              placeholder="비밀번호를 입력하세요"
+              onChange={(e) => {
+                setPwd(e.target.value);
+              }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 1.5 }}
+              onClick={() => {
+                console.log("로그인버튼클릭!");
+                login();
+              }}
+            >
+              로그인하기
+            </Button>
 
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 0.1, mb: 2 }}
+              style={{ backgroundColor: "#FEE500", color: "#000000" }}
+            >
+              카카오 로그인
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  </ThemeProvider>
-  )
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default Login;
